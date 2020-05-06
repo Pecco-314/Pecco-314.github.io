dc0e2bf3328040ab067aecdad331afd4f9825c44
Pecco <pecco@qq.com>
2020-05-06 14:34:59 +0800

    更新字典

--- bilifhx/js/bilifhx.js
+++ bilifhx/js/bilifhx.js
@@ -1,12 +1,40 @@
-const dict1 = ["1/3", "1/5", "73", "535", "586", "1989", "50万", "89年", "三分之一", "五分之一", "你是我", "敏感词", "青鸟衔风", "泽民", "锦涛", "泽东", "小平"];
-const repl1 = ["1/③", "1/⑤", "⑦3", "5③5", "5⑧6", "19⑧9", "五十万", "⑧9年", "三分 之一", "五分 之一", "你是 我", "敏感 词", "青鸟 衔风", "泽 民", "锦 涛", "泽 东", "小 平"];
-const dict2 = [/(包(.*子))/g, /((子.*)包)/g, /(包(.*含))/g, /(包(.*揽))/g, /(吧(.{0,2}九))/g, /(八(.{0,2}九))/g];
-const repl2 = ["bao$2", "$2bao", "bao$2", "bao$2", "吧   $2", "八   $2"];
-const dict3 = ["膜蛤"];
-const repl3 = ["moha"];
+const precision = [
+    ["1/3", "1/③"],
+    ["1/5", "1/⑤"],
+    ["73", "⑦3"],
+    ["535", "5③5"],
+    ["586", "5⑧6"],
+    ["1989", "19⑧9"],
+    ["50万", "五十万"],
+    ["89年", "⑧9年"],
+    ["三分之一", "三分 之一"],
+    ["五分之一", "五分 之一"],
+    ["你是我", "你是 我"],
+    ["敏感词", "敏感 词"],
+    ["青鸟衔风", "青鸟 衔风"],
+    ["泽民", "泽 民"],
+    ["锦涛", "锦 涛"],
+    ["泽东", "泽 东"],
+    ["小平", "小 平"],
+];
+const regex = [
+    [/(包(.*子))/g, "bao$2"],
+    [/((子.*) 包)/g, "$2bao"],
+    [/(包(.*含))/g, "bao$2"],
+    [/(包(.*揽))/g, "bao$2"],
+    [/(吧(.{0,2}九))/g, "吧   $2"],
+    [/(八(.{0,2}九))/g, "八   $2"],
+    [/(([DdＤｄ][iIｉＩ])[cCｃＣ]([kKｋＫ]))/g, "$2*$3"],
+    [/((?<![学练演])习)/g, "刁"],
+    [/(近(\[^\p{Unified_Ideograph}\w])平)/g, "近| $2平"],
+    [/(近(.?)平)/g, "近| $2平"],
+];
+const danger = [
+    ["膜蛤", "moha"]
+];
 
 $(function () {
-    const len = dict1.length + dict2.length + dict3.length;
+    const len = precision.length + regex.length + danger.length;
     $("#stat").text("现收录" + len + "条敏感词");
     $("#confirm-submit-button").on("click", contribute);
 });
@@ -15,17 +43,20 @@ function check() {
     let raw_text = $("#input-textarea").html().replace(/<div>(.*?)<\/div>/g, "<br />$1").replace(/<span.*?>(.*?)<\/span>/g, "$1");
     let warning_text = raw_text,
         replace_text = raw_text;
-    for (let i = 0; i < dict1.length; ++i) {
-        warning_text = warning_text.replace(dict1[i], "<span style=\"background-color:yellow;\">" + dict1[i] + "</span>");
-        replace_text = replace_text.replace(dict1[i], repl1[i]);
+    for (let i = 0; i < precision.length; ++i) {
+        const cur = precision[i];
+        warning_text = warning_text.replace(cur[0], "<span style=\"background-color:yellow;\">" + cur[0] + "</span>");
+        replace_text = replace_text.replace(cur[0], cur[1]);
     }
-    for (let i = 0; i < dict2.length; ++i) {
-        warning_text = warning_text.replace(dict2[i], "<span style=\"background-color:#ff9f9f;\">$1</span>");
-        replace_text = replace_text.replace(dict2[i], repl2[i]);
+    for (let i = 0; i < regex.length; ++i) {
+        const cur = regex[i];
+        warning_text = warning_text.replace(cur[0], "<span style=\"background-color:#ff9f9f;\">$1</span>");
+        replace_text = replace_text.replace(cur[0], cur[1]);
     }
-    for (let i = 0; i < dict3.length; ++i) {
-        warning_text = warning_text.replace(dict3[i], "<span style=\"background-color:#ff4545;\">" + dict3[i] + "</span>");
-        replace_text = replace_text.replace(dict3[i], repl3[i]);
+    for (let i = 0; i < danger.length; ++i) {
+        const cur = danger[i];
+        warning_text = warning_text.replace(cur[0], "<span style=\"background-color:#ff4545;\">" + cur[0] + "</span>");
+        replace_text = replace_text.replace(cur[0], cur[1]);
     }
     $("#input-textarea").html(warning_text);
     $("#result-textarea").html(replace_text);
